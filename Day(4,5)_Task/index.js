1ffa315910f132101f5b2f52d58596146df70494
AYA OBEIDAT <amobeidat208@cit.just.edu.jo>
2025-04-09 17:04:41 +0300

    Add showAllBooks function to display sorted book titles

--- Day(4,5)_Task/index.js
+++ Day(4,5)_Task/index.js
@@ -16,3 +16,22 @@ async function getAllBooks() {
     return null;
   }
 }
+
+async function showAllBooks() {
+    try {
+      const books = await getAllBooks();
+      if (books && books.length > 0) {
+        let bookTitles = books.map(book => book.title)
+                              .sort((a, b) => b.length - a.length);
+        let cleanBookList = [...new Set(bookTitles)];
+        console.log(`-- Book List :\n`);
+        cleanBookList.forEach(title => {
+          console.log(`-- ${title}`);
+        });
+      } else {
+        console.log('No books found');
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  }
